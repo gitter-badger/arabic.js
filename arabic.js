f84933a88516c3eb3d4e47e7fd3ffbb7f9d6ec15
Mohamed Elkebir <elkebir.med@gmail.com>
2015-10-17 23:55:18 +0100

    more comments at the main file

--- arabic.js
+++ arabic.js
@@ -2,8 +2,8 @@
 
 
   // Arabic.js 0.0.1
-  // http://arabicjs.com
-  // (c) 2015 Mohamed Elkebir @elkebirmed
+  // http://elkebirmed.github.io/arabic.js/
+  // (c) 2015 Mohamed Elkebir <elkebir.med@gmail.com>
   // Arabic.js may be freely distributed under the MIT license.
 
 
@@ -20,6 +20,7 @@
     };
 
 
+    // Prototype methods.
     A.prototype = {
 
       // Replace additional white spaces with one space character,
@@ -30,6 +31,8 @@
       },
 
 
+      // Get just the Arabic letters and remove the other characters,
+      // or replace them with the `replacement` if it's defined.
       getArabic: function( replacement ) {
         var replacement = replacement || ' ';
         var value = this.value.replace(/[^\u0621-\u0652]/gm, replacement);
@@ -37,6 +40,8 @@
       },
 
 
+      // Remove Arabic letters from the value, or replace it with
+      // the `replacement` if it's defined, and output the rest characters.
       getNotArabic: function( replacement ) {
         var replacement = replacement || ' ';
         var value = this.value.replace(/[\u0621-\u0652]/gm, replacement);
@@ -63,7 +68,7 @@
       },
 
 
-      // Remove vowels from text, if @hard if true it will removes HAMZA's.
+      // Remove vowels from value.
       removeVowels: function( hard ) {
         var value = this.value.replace(/[\u064B-\u0652]/gm, '');
         return new A(value);
@@ -76,11 +81,13 @@
   }());
 
 
+  // Object wrapper.
   var wrap = function( value ) {
     return new A(value);
   };
 
 
+  // Exporting the library.
   if (typeof window !== 'undefined' && window !== null) {
     window.A = wrap;
   } else {
