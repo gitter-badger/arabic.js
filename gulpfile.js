3ee64bd5ceffc6c4cdc92057a76f2d1495a03899
Mohamed Elkebir <elkebir.med@gmail.com>
2015-10-19 06:46:50 +0100

    commenting gulpfile.js and delete the default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
+// Requiring the Gulp module and some plugins.
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify');
 
 
+// Information.
+var destDir = 'dist',
+    srcFile = './src/arabic.js';
+
+// Linting the main JavaScript file with JsHint.
 gulp.task('lint', function() {
-  return gulp.src('./src/arabic.js')
+  return gulp.src(srcFile)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 
+// Compressing the main JavaScript file with Uglify.
 gulp.task('compress', function() {
-  return gulp.src('./src/arabic.js')
+  return gulp.src(srcFile)
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(destDir));
 });
 
 
-gulp.task('default', ['lint', 'compress']);
+// Tasks
+gulp.task('build', ['lint', 'compress']);
